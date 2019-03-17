0351c260c6d96d712321006f6b612a8e0a135ddc
Diego Vizhnay <diego@vzhny.com>
2019-03-17 18:38:23 -0400

    📝 Changed refactor again (!), added styles, tests | 03/17

--- git-emoji-commit.sh
+++ git-emoji-commit.sh
@@ -3,13 +3,15 @@
 echo -e "\n 👉  Select your emoji from the following list:\n"
 echo -e "\t 🎉  (i)nitial"
 echo -e "\t 📝  (n)ew"
-echo -e "\t ♻  (r)efactor"
+echo -e "\t 🔨  (r)efactor"
+echo -e "\t 🎨  (s)tyles"
 echo -e "\t 🔥  (d)elete"
 echo -e "\t 🚚  (m)ove"
 echo -e "\t 📖  d(o)c"
 echo -e "\t 🐛  (b)ugfix"
 echo -e "\t ➕  (a)dd"
 echo -e "\t ➖  r(e)move"
+echo -e "\t 👷  (t)ests"
 
 emoji=😮
 
@@ -20,13 +22,15 @@ read -p "Selection: " emoji_selection
 case $emoji_selection in
   i ) emoji=🎉;;
   n ) emoji=📝;;
-  r ) emoji=♻;;
+  r ) emoji=🔨;;
+  s ) emoji=🎨;;
   d ) emoji=🔥;;
   m ) emoji=🚚;;
   o ) emoji=📖;;
   b ) emoji=🐛;;
   a ) emoji=➕;;
   e ) emoji=➖;;
+  t ) emoji=👷;;
   * ) 
     echo -e "$emoji No valid emoji was selected. Exiting."
     exit 1
